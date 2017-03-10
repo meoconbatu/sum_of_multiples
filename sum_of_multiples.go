e38127ee08eb19920523ee6374c0a8978b0036a8
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-03-10 21:06:29 +0700

    optimize: use map[int]bool instead of []int

--- sum_of_multiples.go
+++ sum_of_multiples.go
@@ -1,28 +1,14 @@
 package summultiples
 
-import "sort"
-
 func SumMultiples(limit int, divisors ...int) (sum int) {
-	var multiples []int
+	multiples := make(map[int]bool, limit)
 	for _, divisor := range divisors {
-		multiples = append(multiples, GetMultiplesUpTo(divisor, limit)...)
-	}
-	sort.Ints(multiples)
-	numMultiples := len(multiples)
-	for i := 0; i < numMultiples; i++ {
-		if i+1 == numMultiples {
-			return sum + multiples[i]
+		for i := divisor; i < limit; i += divisor {
+			if multiples[i] == false {
+				sum += i
+				multiples[i] = true
+			}
 		}
-		if multiples[i] != multiples[i+1] {
-			sum += multiples[i]
-		}
-	}
-	return
-}
-
-func GetMultiplesUpTo(divisor int, limit int) (multiples []int) {
-	for i := 0; i*divisor < limit; i++ {
-		multiples = append(multiples, i*divisor)
 	}
 	return
 }
